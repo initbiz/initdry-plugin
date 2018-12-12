cdc3d0fefc36901d77fdfd65ea6ba0ebd445a95a
Tomasz Strojny <tomasz@init.biz>
2018-12-12 14:59:23 +0100

    Added str_random method and stringhelpers

---
+++ classes/StringHelpers.php
@@ -0,0 +1,24 @@
+<?php namespace Initbiz\InitDry\Classes;
+
+class StringHelpers
+{
+    /**
+     * Method by Scott Arciszewski: https://stackoverflow.com/a/31107425
+     * Generate a random string, using a cryptographically secure
+     * pseudorandom number generator (random_int)
+     *
+     * @param int $length      How many characters do we want?
+     * @param string $keyspace A string of all possible characters
+     *                         to select from
+     * @return string
+     */
+    public static function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
+}
