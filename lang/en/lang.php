25647d95787c1d55a441b9a146c42d8ec32ac952
Tomasz Strojny <tomasz@init.biz>
2019-10-07 12:22:33 +0200

    Added pl lang

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,4 +3,7 @@
         'name' => 'InitDry',
         'description' => 'InIT.biz package with useful classes and helpers',
     ],
+    'confirm' => [
+        'delete_confirm' => 'Are you sure you want to delete the checked records?',
+    ],
 ];
---
+++ lang/pl/lang.php
@@ -0,0 +1,9 @@
+<?php return [
+    'plugin' => [
+        'name' => 'InitDry',
+        'description' => 'Paczka InIT.biz z przydatnymi klasami i helperami',
+    ],
+    'confirm' => [
+        'delete_confirm' => 'Czy jesteś pewny, że chcesz usunąć zaznaczone elementy?'
+    ],
+];
