434c26581a228c1961210a4b061755c82bf6f073
Tomasz Strojny <tomasz@init.biz>
2020-08-27 16:51:36 +0200

    Reboot CSS feature

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,11 @@
 <?php namespace Initbiz\InitDry;
 
+use File;
+use Cache;
+use Config;
+use Cms\Classes\Theme;
 use System\Classes\PluginBase;
+use System\Classes\CombineAssets;
 use System\Classes\PluginManager;
 
 /**
@@ -8,6 +13,8 @@ use System\Classes\PluginManager;
  */
 class Plugin extends PluginBase
 {
+    static $rebootCssCacheKey = 'reboot-css.last-modified';
+
     public function register()
     {
         $this->registerConsoleCommand('initdry.droptables', 'Initbiz\InitDry\Console\DropTables');
@@ -19,6 +26,7 @@ class Plugin extends PluginBase
         return [
             'functions' => [
                 'hasPlugin' => [$this, 'hasPlugin'],
+                'rebootingStyles' => [$this, 'rebootingStyles'],
             ]
         ];
     }
@@ -33,4 +41,38 @@ class Plugin extends PluginBase
 
         return $pluginManager->hasPlugin($pluginCode) && !$pluginManager->isDisabled($pluginCode);
     }
+
+    public function rebootingStyles()
+    {
+        $src = Config::get('initbiz.initdry::rebootScssPath');
+        $destination = Config::get('initbiz.initdry::rebootCssPath');
+
+        $fileExists = File::exists($destination);
+        if ($fileExists) {
+            $theme = Theme::getActiveTheme();
+            $path = $theme->getPath() . $src;
+            $lastMod = File::lastModified($path);
+            if ($lastMod !== Cache::get(Self::$rebootCssCacheKey)) {
+                $this->compileRebootCss();
+                $lastMod = File::lastModified($path);
+                Cache::forever(Self::$rebootCssCacheKey, $lastMod);
+            }
+        } else {
+            $this->compileRebootCss();
+        }
+
+        return File::get($destination);
+    }
+
+    protected function compileRebootCss()
+    {
+        $combiner = CombineAssets::instance();
+        $destination = Config::get('initbiz.initdry::rebootCssPath');
+        $src = Config::get('initbiz.initdry::rebootScssPath');
+
+        $theme = Theme::getActiveTheme();
+        $path = $theme->getPath() . $src;
+        $combiner->combineToFile([$path], $destination);
+    }
+
 }
