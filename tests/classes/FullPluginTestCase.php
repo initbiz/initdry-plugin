1136ac229f6f073a9d45cc139baa90596aed0f63
Tomasz Strojny <tomasz@init.biz>
2022-07-03 16:42:24 +0200

    Update FullPluginTestCase to reflect the newest PluginTestCase

--- tests/classes/FullPluginTestCase.php
+++ tests/classes/FullPluginTestCase.php
@@ -6,8 +6,6 @@ use Storage;
 use PluginTestCase;
 use October\Rain\Database\Model;
 use System\Classes\PluginManager;
-use System\Classes\UpdateManager;
-use System\Classes\VersionManager;
 
 abstract class FullPluginTestCase extends PluginTestCase
 {
@@ -17,18 +15,11 @@ abstract class FullPluginTestCase extends PluginTestCase
 
         Storage::fake('local');
 
-        // Version manager remembers in the databaseVersions all versions
-        // between tests even if the table in db is empty
-        VersionManager::forgetInstance();
-
         // Get the plugin manager
         $pluginManager = PluginManager::instance();
 
         Model::clearExtendedClasses();
         Model::clearBootedModels();
-
-        // TODO: Ensure this line is not necessary to run all types of tests
-        // Model::flushEventListeners();
 
         // Register the plugins to make features like file configuration available
         $pluginManager->registerAll(true);
@@ -46,14 +37,5 @@ abstract class FullPluginTestCase extends PluginTestCase
 
         // Ensure that plugins are registered again for the next test
         $pluginManager->unregisterAll();
-    }
-
-    protected function runPluginRefreshCommand($code, $throwException = true)
-    {
-        // Plugin refresh does not migrate all of the tables
-        // That's why we're running update here so that all migrations
-        // will be run by plugin:refresh command
-        UpdateManager::instance()->updatePlugin($code);
-        parent::runPluginRefreshCommand($code, $throwException);
     }
 }
