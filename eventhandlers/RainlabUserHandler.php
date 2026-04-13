9c16462dfddd8e481b0b53edbf683db6ff12a6a8
Mateusz Rycombel <mateusz@init.biz>
2026-04-13 15:37:39 +0200

    add accessor and mutator for user name and surname

---
+++ eventhandlers/RainlabUserHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Initbiz\InitDry\EventHandlers;
+
+use App;
+use Lang;
+use System;
+use RainLab\User\Models\User;
+use RainLab\User\Controllers\Users;
+use Initbiz\CumulusCore\Models\Cluster;
+
+class RainlabUserHandler
+{
+    public function subscribe($event)
+    {
+        $this->addNameAndSurnameAccessor($event);
+    }
+
+    public function addNameAndSurnameAccessor($event)
+    {
+        User::extend(function ($model) {
+
+            $model->addDynamicMethod('getNameAttribute', function () use ($model) {
+                return $model->first_name;
+            });
+
+            $model->addDynamicMethod('getSurnameAttribute', function () use ($model) {
+                return $model->last_name;
+            });
+
+            $model->addDynamicMethod('setNameAttribute', function ($value) use ($model) {
+                $model->first_name = $value;
+            });
+
+            $model->addDynamicMethod('setSurnameAttribute', function ($value) use ($model) {
+                $model->last_name = $value;
+            });
+        });
+    }
+}
