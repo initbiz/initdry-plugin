a7e46e43f9773ecc8dbccae67f43ac19c6ee6742
Patryk Trąd <patryk@init.biz>
2026-04-14 11:49:31 +0200

    add linter

---
+++ .danger.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Initbiz\Linter\Classes\DangerConfigMaker;
+
+include_once 'classes/DangerConfigMaker.php';
+
+$configMaker = new DangerConfigMaker();
+
+// Enable rules below
+
+$configMaker->enableRule('dangerrules/VersionYamlUpdatedRule.php');
+// TODO: rules
+// - merge request's source branch name starts with feature/, or bugfix/
+// - merge request name starts with task ID e.g.: #1234:
+
+return $configMaker->getConfig();
