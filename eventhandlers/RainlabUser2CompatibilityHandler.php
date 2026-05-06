52d68d35d9cef22ef15619f0257ae8876a531623
wojciech <wojciech@init.biz>
2026-05-06 11:15:10 +0200

    Update RainlabUser2CompatibilityHandler

--- eventhandlers/RainlabUser2CompatibilityHandler.php
+++ eventhandlers/RainlabUser2CompatibilityHandler.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace Initbiz\InitDry\EventHandlers;
 
+use Schema;
 use RainLab\User\Models\User;
 
 class RainlabUser2CompatibilityHandler
 {
     public function subscribe($event)
     {
-        if (\Schema::hasColumn('users', 'first_name')) {
+        // RainLab.User v3 compatibility
+        if (Schema::hasColumn('users', 'first_name')) {
             $this->addNameAndSurnameAccessor($event);
+        }
+
+        // RainLab.User v2 compatibility
+        if (!Schema::hasColumn('users', 'first_name')) {
+            $this->addFirstNameAndLastNameAccessor($event);
+        }
+
+        if (Schema::hasColumn('users', 'is_activated')) {
+            $this->addActivatedAtAccessor($event);
         }
     }
 
+    // RainLab.User v3 compatibility
     public function addNameAndSurnameAccessor($event)
     {
         User::extend(function ($model) {
@@ -33,6 +45,41 @@ class RainlabUser2CompatibilityHandler
 
             $model->addDynamicMethod('setSurnameAttribute', function ($value) use ($model) {
                 $model->last_name = $value;
+            });
+        });
+    }
+
+    // RainLab.User v2 compatibility
+    public function addFirstNameAndLastNameAccessor($event)
+    {
+        User::extend(function ($model) {
+
+            $model->addDynamicMethod('getFirstNameAttribute', function () use ($model) {
+                return $model->name;
+            });
+
+            $model->addDynamicMethod('getLastNameAttribute', function () use ($model) {
+                return $model->surname;
+            });
+
+            $model->addDynamicMethod('setFirstNameAttribute', function ($value) use ($model) {
+                $model->name = $value;
+            });
+
+            $model->addDynamicMethod('setLastNameAttribute', function ($value) use ($model) {
+                $model->surname = $value;
+            });
+        });
+    }
+
+    public function addActivatedAtAccessor($event)
+    {
+        User::extend(function ($model) {
+            $model->addDynamicMethod('setActivatedAtAttribute', function ($value) use ($model) {
+                if (!is_null($value)) {
+                    $model->activated_at = $value;
+                    $model->is_activated = true;
+                }
             });
         });
     }
